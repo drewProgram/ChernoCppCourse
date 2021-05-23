a56e007cb040fb4009ffad8ee9b0e56f75ee5ea4
Andrew Filgueiras <andrew.lascasas@gmail.com>
2021-05-23 15:15:35 -0300

    reorginized projects and folders structure

---
+++ _37StaticArrays/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <array>
+
+void PrintArray(const std::array<int, 5>& data)
+{
+
+}
+
+int main()
+{
+	std::array<int, 5> data;
+	data[0] = 2;
+	data[4] = 1;
+
+	std::cin.get();
+}
